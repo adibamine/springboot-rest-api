b5e82d71ab4c279514504f6709fb6b31b08bd48b
adibamine <adib.amine.mdr@gmail.com>
2017-03-07 15:18:49 +0100

    Update Application.java

--- src/main/java/com/adib/Application.java
+++ src/main/java/com/adib/Application.java
@@ -3,7 +3,7 @@ package com.adib;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.adib.*")
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
